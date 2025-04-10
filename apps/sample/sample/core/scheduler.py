6d7cd31a55e8ce75913a062db09eeba5fb120b94
Niels P. Sorensen <sorensen.niels@gmail.com>
2025-04-10 14:16:00 +0000

    Updated dependencies

--- apps/sample/sample/core/scheduler.py
+++ apps/sample/sample/core/scheduler.py
@@ -1,7 +1,5 @@
 from datetime import datetime, timedelta, timezone
-import time
 import uuid
-from apscheduler.executors.pool import ProcessPoolExecutor, ThreadPoolExecutor
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from motor.motor_asyncio import AsyncIOMotorDatabase
 from sample.models.job_lock_beanie import JobLock
